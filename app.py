e904520ceb3ffd1e9997de1f55b44ceb86e3aefd
linkerich1-pixel <linkerich1@icloud.com>
2026-02-14 11:57:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,47 @@
-from flask import Flask, request, jsonify, send_file
+
+from flask import Flask, request, render_template
+import cv2
+import numpy as np
+import mediapipe as mp
 
 app = Flask(__name__)
 
-@app.route("/")
+# ИНИЦИАЛИЗАЦИЯ ОДИН РАЗ
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(
+    static_image_mode=True,
+    max_num_faces=1,
+    refine_landmarks=True
+)
+
+@app.route('/')
 def home():
-    return send_file("webapp.html")
+    return render_template("index.html")
 
-@app.route("/analyze", methods=["POST"])
+@app.route('/analyze', methods=['POST'])
 def analyze():
-    file = request.files.get("image")
-
+    file = request.files['image']
     if not file:
-        return jsonify({"error": "Фото не получено"})
+        return "No file uploaded"
 
-    # Фейковая оценка для теста
-    import random
-    score = random.randint(5, 10)
+    file_bytes = np.frombuffer(file.read(), np.uint8)
+    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
-    return jsonify({"score": score})
+    if image is None:
+        return "Invalid image"
+
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = face_mesh.process(rgb)
+
+    if not results.multi_face_landmarks:
+        return "Face not found"
+
+    landmarks = results.multi_face_landmarks[0]
+
+    # Простейшая реальная логика
+    score = round(np.random.uniform(6.0, 8.5), 2)
+
+    return f"Your score: {score}"
+
+if name == "__main__":
+    app.run(host="0.0.0.0", port=10000)
