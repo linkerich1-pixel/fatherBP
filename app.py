a0c1aeacd13f803e36ee70652f4c6d80ee2eea7c
linkerich1-pixel <linkerich1@icloud.com>
2026-02-14 11:15:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask, send_file
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return send_file("webapp.html")
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
-
