898570876edc629ebb67572e2c0a8f00e7c45233
linkerich1-pixel <linkerich1@icloud.com>
2026-02-14 10:58:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Server is running"
-
-@app.route("/webapp")
-def webapp():
-    return render_template("webapp.html")
-
-@app.route("/analyze", methods=["POST"])
-def analyze():
-    return jsonify({
-        "score": 7.8,
-        "advice": "Good base. Improve hairstyle and reduce face fat."
-    })
+    return "WORKING 🔥"
 
 if name == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
